2470d01aba19ee70e49a8b84a8effffbd2d06f5f
Alexandros G. Keramidas <dev.alexkeramidas@gmail.com>
2016-11-29 18:33:45 +0200

    Fixed some issues with nil value exceptions

--- lib/omniauth/strategies/authentiq.rb
+++ lib/omniauth/strategies/authentiq.rb
@@ -30,24 +30,24 @@ module OmniAuth
 
       info do
         {
-            :name => @raw_info['name'],
-            :first_name => @raw_info['given_name'],
-            :last_name => @raw_info['family_name'],
-            :email => @raw_info['email'],
-            :phone => @raw_info['phone_number'],
-            :location => @raw_info['address']['country']
-        }
+            :name => (@raw_info['name'] unless @raw_info['name'].nil?),
+            :first_name => (@raw_info['given_name'] unless @raw_info['given_name'].nil?),
+            :last_name => (@raw_info['family_name'] unless @raw_info['family_name'].nil?),
+            :email => (@raw_info['email'] unless @raw_info['email'].nil?),
+            :phone => (@raw_info['phone_number'] unless @raw_info['phone_number'].nil?),
+            :location => (@raw_info['address'] unless @raw_info['address'].nil?),
+            :geolocation => (@raw_info['location'] unless @raw_info['location'].nil?)
+        }.reject { |k, v| v.nil? }
       end
 
       extra do
         {
-            :middle_name => @raw_info['middle_name'],
-            :email_verified => @raw_info['email_verified'],
-            :phone_type => @raw_info['phone_type'],
-            :phone_number_verified => @raw_info['phone_number_verified'],
-            :locale => @raw_info['locale'],
-            :zoneinfo => @raw_info['zoneinfo']
-        }
+            :middle_name => (@raw_info['middle_name'] unless @raw_info['middle_name'].nil?),
+            :email_verified => (@raw_info['email_verified'] unless @raw_info['email_verified'].nil?),
+            :phone_type => (@raw_info['phone_type'] unless @raw_info['phone_type'].nil?),
+            :phone_number_verified => (@raw_info['phone_number_verified'] unless @raw_info['phone_number_verified'].nil?),
+            :locale => (@raw_info['locale'] unless @raw_info['locale'].nil?)
+        }.reject { |k, v| v.nil? }
       end
 
       def raw_info
@@ -62,7 +62,6 @@ module OmniAuth
         # Fixes regression in omniauth-oauth2 v1.4.0 by https://github.com/intridea/omniauth-oauth2/commit/85fdbe117c2a4400d001a6368cc359d88f40abc7
         options[:callback_url] || (full_host + script_name + callback_path)
       end
-
     end
   end
 end
